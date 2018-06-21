b731eebcfc01fbf7e32bd040bb88511eac7daae6
shanbaker <36170850+shanbaker@users.noreply.github.com>
2018-06-21 01:50:07 -0400

    updated js

--- app.js
+++ app.js
@@ -16,18 +16,36 @@ function onReady() {
        // set the input's type to checkbox
        checkbox.type = "checkbox";
 
-         // set the title
+       // set the title
     newLi.textContent = title;
 
-    // attach the checkbox to the li
+      // attach the checkbox to the li
     newLi.appendChild(checkbox);
 
-    // attach the li to the ul
+      // attach the li to the ul
     toDoList.appendChild(newLi);
 
-    //empty the input
+      //empty the input
     newToDoText.value = '';
-   
+
+     // add delete button
+     let deletebtn = document.createElement('button');
+
+     // create a text node
+     let deleteinput = document.createTextNode('Delete');
+
+     // append the text to <button>
+     deletebtn.appendChild(deleteinput);
+
+     // append <button> to <form>
+     newLi.appendChild(deletebtn);
+
+      deletebnt.addEventListener('click', function (){
+         let deleteLi = newToDoText.value;
+         toDoList.removeChild(deleteLi);
+     });
+    
+       
 });
 }
 window.onload = function() {
